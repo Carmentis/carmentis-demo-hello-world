10a30940cbb4fb5c7c903204e06f056956e38f0b
Gaël Marcadet <gamarcadet@gmail.com>
2026-09-24 14:54:25 +0200

    BREAKING CHANGE: Application is now a playground for testing Desk

--- src/data/json-rpc-templates.ts
+++ src/data/json-rpc-templates.ts
@@ -18,76 +18,20 @@ export const jsonRpcTemplates: JsonRpcTemplate[] = [
     },
   },
   {
-    id: 'get-application',
-    title: 'Get Application',
-    description: 'Retrieve information about an application',
+    id: 'sign-json-1',
+    title: 'Sign JSON #1',
+    description: 'Ask Desk to sign a JSON',
     request: {
       jsonrpc: '2.0',
       id: 1,
-      method: 'getApplication',
+      method: '/v1/sign/json',
       params: {
-        applicationId: '',
-      },
-    },
-  },
-  {
-    id: 'list-applications',
-    title: 'List Applications',
-    description: 'Get a list of all applications',
-    request: {
-      jsonrpc: '2.0',
-      id: 1,
-      method: 'listApplications',
-      params: {},
-    },
-  },
-  {
-    id: 'get-module',
-    title: 'Get Module',
-    description: 'Retrieve information about a specific module',
-    request: {
-      jsonrpc: '2.0',
-      id: 1,
-      method: 'getModule',
-      params: {
-        moduleId: '',
-      },
-    },
-  },
-  {
-    id: 'list-modules',
-    title: 'List Modules',
-    description: 'Get a list of all modules',
-    request: {
-      jsonrpc: '2.0',
-      id: 1,
-      method: 'listModules',
-      params: {},
-    },
-  },
-  {
-    id: 'create-anchor',
-    title: 'Create Anchor',
-    description: 'Create a new anchor with metadata',
-    request: {
-      jsonrpc: '2.0',
-      id: 1,
-      method: 'createAnchor',
-      params: {
-        metadata: {},
-      },
-    },
-  },
-  {
-    id: 'get-anchor',
-    title: 'Get Anchor',
-    description: 'Retrieve information about a specific anchor',
-    request: {
-      jsonrpc: '2.0',
-      id: 1,
-      method: 'getAnchor',
-      params: {
-        anchorId: '',
+        context: {
+          purpose: "Sign JSON"
+        },
+        payload: {
+          message: "Hello, World!"
+        }
       },
     },
   },
